7af968c7bed20deb791d8e0a1633da974775831d
Thomas Hayes <hayes.thomas610@gmail.com>
2022-06-09 21:05:24 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,22 @@
 
 # YOU DO the rest:
 
-
 "Hello World".include?("Hello")
-
+#the include? method is called on the string object "Hello World"
+#the method asks if the word (Hello?) is included in the string and returns with true or false
 
 "Hello World".end_with?("Hello")
-
+#the end_with? method is called on the string object "Hello World"
+#the method asks if the string ends with ("Hello") and returns with true or false
 
 "Hello World".end_with?("rld")
-
+#the end_with? method is called on the string object "Hello World"
+#the method is asking if the end of the string ends with "rld" and resturns with a true or false
 
 12.even?
+#the even? method is called on a integer object "12"
+#the method is asking if the integer is an even number and retruns with a true or false
 
-
-18.next
-
-
+p 18.next
+#the next method is calling on an integer object "18"
+#the method is idenifying the integer and retrurning the next highest whole number in sequence
